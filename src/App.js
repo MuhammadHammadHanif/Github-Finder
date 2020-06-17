1be8221a0f9c88b17d55ce63f565f9d23f5f5e86
Muhammad Hammad <hammadhanif0@gmail.com>
2020-06-17 10:53:15 +0500

    Creating Navbar, User, UserItem Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-  return <div className='App'>Github Finder</div>;
-};
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
